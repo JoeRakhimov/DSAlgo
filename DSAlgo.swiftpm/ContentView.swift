942310d347e799990796618726c8eea57fd3d54a
Joe Rakhimov <joerakhimov@gmail.com>
2024-03-25 15:23:33 +0100

    Added Playground file

---
+++ DSAlgo.swiftpm/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "function")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Let's learn data structures and algorithms!")
+        }
+    }
+}
